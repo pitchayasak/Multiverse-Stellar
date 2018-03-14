f0e1dd764725e10c01b91f28750456f5014c9279
Unknown <pitchayasak.s@gmail.com>
2018-03-14 17:01:16 +0700

    update 23

--- testnet/setup.sh
+++ testnet/setup.sh
@@ -13,11 +13,14 @@ cp src/gce-core-validator.jinja.template template/gce-core-validator.jinja
 cp src/gce-core-validator.yaml.template template/gce-core-validator.yaml
 cp src/gce-core-horizon.jinja.template template/gce-core-horizon.jinja
 cp src/gce-core-horizon.yaml.template template/gce-core-horizon.yaml
+cp src/gce-horizon-ha.jinja.template template/gce-horizon-ha.jinja
+cp src/gce-horizon-ha.yaml.template template/gce-horizon-ha.yaml
 
 cp src/deploy.sh.template deploy.sh
 cp src/core-validator.cfg.template scripts/core-validator.cfg
 cp src/core-watcher.cfg.template scripts/core-watcher.cfg
 cp src/stellar-horizon-ingest.cfg.template scripts/stellar-horizon-ingest.cfg
+cp src/stellar-horizon-ha.cfg.template scripts/stellar-horizon-ha.cfg
 cp src/cloudsql.json scripts/cloudsql.json
 
 sed -i 's/<!GCP_PROJECT_NAME>/'"$GCP_PROJECT_NAME"'/g' deploy.sh
@@ -78,7 +81,27 @@ sed -i 's/<!HISTORY_ARCHIVE>/'"$HISTORY_ARCHIVE"'/g' template/gce-core-horizon.y
 sed -i 's/<!DEPLOYMENT_SCRIPTS>/'"$DEPLOYMENT_SCRIPTS"'/g' template/gce-core-horizon.yaml
 sed -i 's/<!CH_MACHINE_TYPE>/'"$CH_MACHINE_TYPE"'/g' template/gce-core-horizon.yaml
 
+sed -i 's/<!GCP_PROJECT_NAME>/'"$GCP_PROJECT_NAME"'/g' template/gce-horizon-ha.yaml
+sed -i 's/<!CH_GCE_SERVICE_ACCOUNT>/'"$CH_GCE_SERVICE_ACCOUNT"'/g' template/gce-horizon-ha.yaml
+sed -i 's/<!GCP_NETWORK_NAME>/'"$GCP_NETWORK_NAME"'/g' template/gce-horizon-ha.yaml
 
-gsutil cp scripts/* gs://$DEPLOYMENT_SCRIPTS
+sed -i 's/<!CHH_NAME_PREFIX>/'"$CHH_NAME_PREFIX"'/g' template/gce-horizon-ha.yaml
+sed -i 's/<!STELLAR_NETWORK_PASSPHRASE>/'"$STELLAR_NETWORK_PASSPHRASE"'/g' template/gce-horizon-ha.yaml
+
+sed -i 's/<!CORE_HORIZON_ZONE_1>/'"$CORE_HORIZON_ZONE_1"'/g' template/gce-horizon-ha.yaml
+
+sed -i 's/<!CH_PSQL_CORE_DBNAME>/'"$CH_PSQL_CORE_DBNAME"'/g' template/gce-horizon-ha.yaml
+sed -i 's/<!CH_PSQL_HORIZON_DBNAME>/'"$CH_PSQL_HORIZON_DBNAME"'/g' template/gce-horizon-ha.yaml
+sed -i 's/<!CH_PSQL_USERNAME>/'"$CH_PSQL_USERNAME"'/g' template/gce-horizon-ha.yaml
+sed -i 's/<!CH_PSQL_PASSWORD>/'"$CH_PSQL_PASSWORD"'/g' template/gce-horizon-ha.yaml
+
+sed -i 's/<!CLOUD_SQL_INSTANCE>/'"$CLOUD_SQL_INSTANCE"'/g' template/gce-horizon-ha.jinja
+
+sed -i 's/<!HISTORY_ARCHIVE>/'"$HISTORY_ARCHIVE"'/g' template/gce-horizon-ha.yaml
+sed -i 's/<!DEPLOYMENT_SCRIPTS>/'"$DEPLOYMENT_SCRIPTS"'/g' template/gce-horizon-ha.yaml
+sed -i 's/<!CH_MACHINE_TYPE>/'"$CH_MACHINE_TYPE"'/g' template/gce-horizon-ha.yaml
+
+
+gsutil -m cp scripts/* gs://$DEPLOYMENT_SCRIPTS
 
 gsutil acl ch -u $GCE_SERVICE_ACCOUNT:W gs://$HISTORY_ARCHIVE
